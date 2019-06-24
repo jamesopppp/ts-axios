84d4d0694f23040f68a9fcc840834bb1f3c4c307
James's MacBook <jamessupermail@163.com>
2019-06-24 22:37:08 +0800

    feat(all the files): add some feat
    add some feat

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
-function axios(config: AxiosRequestConfig): void {
-  // TODO
-  xhr(config)
+function axios(config: AxiosRequestConfig): AxiosPromise {
+  processConfig(config)
+  return xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig) {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
